66f0363b62ad1fc2db30bae4c132b3d4c12341b1
YadavYashvant <yashvantyadav855@gmail.com>
2023-09-01 21:35:48 +0530

    Initial commit

---
+++ dialog-lib/src/main/java/com/example/dialogsy/Dialogsy.kt
@@ -0,0 +1,9 @@
+package com.example.dialogsy
+
+import android.content.Context
+
+public class Dialog {
+    public fun Dialogsy(Title:String, Message:String, ConfirmBtnText: String, DismissBtnText: String) {
+
+    }
+}
